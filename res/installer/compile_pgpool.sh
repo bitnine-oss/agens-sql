18c32949365678aa06ed906180bec1cda714ac63
mesh <mesh@localhost.localdomain>
2015-05-29 20:40:29 +0900

    fix about pgpool

--- res/installer/compile_pgpool.sh
+++ res/installer/compile_pgpool.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-
-# check root privileges
-if [ $(id -u) != "0" ]; then
-	echo "This script requires root privileges."
-	exit 1
-fi
-
-# bin
-chmod a+x -R $INSTALL_PATH/pgpool/bin
-
-# lib
-rm $INSTALL_PATH/pgpool/lib/libpcp.so
-rm $INSTALL_PATH/pgpool/lib/libpcp.so.0
-chmod 755 $INSTALL_PATH/pgpool/lib/libpcp.la
-chmod 755 $INSTALL_PATH/pgpool/lib/libpcp.so.0.0.0
-ln -s libpcp.so.0.0.0 $INSTALL_PATH/pgpool/lib/libpcp.so
-ln -s libpcp.so.0.0.0 $INSTALL_PATH/pgpool/lib/libpcp.so.0
-
-
-
+#make symlink
+ln -s $INSTALL_PATH/pgpool/lib/libpcp.so.0.0.0 $INSTALL_PATH/pgpool/lib/libpcp.so
+ln -s $INSTALL_PATH/pgpool/lib/libpcp.so.0.0.0 $INSTALL_PATH/pgpool/lib/libpcp.so.0
