c8c6e3de205727f4e6a98586d2fa8406cf85e1c8
mesh <mesh@localhost.localdomain>
2015-06-08 10:51:51 +0900

    update resources

--- res/installer/compile_pgsql.sh
+++ res/installer/compile_pgsql.sh
@@ -3,45 +3,42 @@
 if [ ! -f $INSTALL_PATH/pgsql/bin/postmaster ]; then ln -s $INSTALL_PATH/pgsql/bin/postgres $INSTALL_PATH/pgsql/bin/postmaster; fi
 
 AGENS_HOME="$INSTALL_PATH"
-agens_passwd_file.$$=$AGENS_HOME/pgsql/dbuser_passwd
+agens_passwd_file=$AGENS_HOME/pgsql/dbuser_passwd
 password="$agens.password"
-port="$agens-sql.port"
-DATA_DIR="$agens-sql.data_path"
+port="$agens_sql.port"
+DATA_DIR="$agens_sql.data_path"
 
 # create password file
-echo "$password" > $agens_passwd_file.$$
-chmod 600 $agens_passwd_file.$$
+echo "$password" > $agens_passwd_file
+chmod 600 $agens_passwd_file
 
 # initialize agens database
 if [ -z $password ]; then
 	LD_LIBRARY_PATH=$AGENS_HOME/pgsql/lib $AGENS_HOME/pgsql/bin/initdb -U agens -D $DATA_DIR
 else
-	LD_LIBRARY_PATH=$AGENS_HOME/pgsql/lib $AGENS_HOME/pgsql/bin/initdb --pwfile=$agens_passwd_file.$$ -A md5 -U agens -D $DATA_DIR
+	LD_LIBRARY_PATH=$AGENS_HOME/pgsql/lib $AGENS_HOME/pgsql/bin/initdb --pwfile=$agens_passwd_file -A md5 -U agens -D $DATA_DIR
 fi
 
 # edit postgresql.conf
 if [ $port -ne "5456" ]; then
-    sed -e 's/#port = 5456/port = $agens-sql.port/g' $DATA_DIR/postgresql.conf > $DATA_DIR/postgresql.conf.$$
-    mv $DATA_DIR/postgresql.conf.$$ $DATA_DIR/postgresql.conf
+    sed -e 's/#port = 5456/port = $agens_sql.port/g' $DATA_DIR/postgresql.conf > $DATA_DIR/postgresql.conf
+    mv $DATA_DIR/postgresql.conf $DATA_DIR/postgresql.conf
     chmod 600 $DATA_DIR/postgresql.conf
 fi
 
 
 # operate server
-LD_LIBRARY_PATH=$AGENS_HOME/pgsql/lib $AGENS_HOME/pgsql/bin/pg_ctl -D $DATA_DIR -l $DATA_DIR/server_log.txt start
-
-# wait for booting postmaster process
-sleep 3
+LD_LIBRARY_PATH=$AGENS_HOME/pgsql/lib $AGENS_HOME/pgsql/bin/pg_ctl -w -D $DATA_DIR -l $DATA_DIR/server_log.txt start
 
 # createdb
 if [ -z $password ]; then
 	$AGENS_HOME/pgsql/bin/createdb -p $port -U agens agens
 else
-	$AGENS_HOME/pgsql/bin/createdb -p $port -U agens agens < $agens_passwd_file.$$
+	$AGENS_HOME/pgsql/bin/createdb -p $port -U agens agens < $agens_passwd_file
 fi
 
 # remove password file
-rm -f $agens_passwd_file.$$
+rm -f $agens_passwd_file
 
 
 # run psql
