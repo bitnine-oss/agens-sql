c0c9f5047f3e04871153124c98aae503b518bb11
mesh <mesh@localhost.localdomain>
2015-06-10 09:43:49 +0900

    improve UI / fix symlink pgsql/lib

--- res/installer/compile_pgsql.sh
+++ res/installer/compile_pgsql.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 if [ ! -f $INSTALL_PATH/pgsql/bin/postmaster ]; then ln -s $INSTALL_PATH/pgsql/bin/postgres $INSTALL_PATH/pgsql/bin/postmaster; fi
+if [ ! -f $INSTALL_PATH/pgsql/lib/libecpg.so.6 ]; then ln -s $INSTALL_PATH/pgsql/lib/libecpg.so.6.6 $INSTALL_PATH/pgsql/lib/libecpg.so.6; fi
+if [ ! -f $INSTALL_PATH/pgsql/lib/libecpg.so ]; then ln -s $INSTALL_PATH/pgsql/lib/libecpg.so.6.6 $INSTALL_PATH/pgsql/lib/libecpg.so; fi
+if [ ! -f $INSTALL_PATH/pgsql/lib/libecpg_compat.so.3 ]; then ln -s $INSTALL_PATH/pgsql/lib/libecpg_compat.so.3.6 $INSTALL_PATH/pgsql/lib/libecpg_compat.so.3; fi
+if [ ! -f $INSTALL_PATH/pgsql/lib/libecpg_compat.so ]; then ln -s $INSTALL_PATH/pgsql/lib/libecpg_compat.so.3.6 $INSTALL_PATH/pgsql/lib/libecpg_compat.so; fi
+if [ ! -f $INSTALL_PATH/pgsql/lib/libpgtypes.so ]; then ln -s $INSTALL_PATH/pgsql/lib/libpgtypes.so.3.5 $INSTALL_PATH/pgsql/lib/libpgtypes.so; fi
+if [ ! -f $INSTALL_PATH/pgsql/lib/libpgtypes.so.3 ]; then ln -s $INSTALL_PATH/pgsql/lib/libpgtypes.so.3.5 $INSTALL_PATH/pgsql/lib/libpgtypes.so.3; fi
+if [ ! -f $INSTALL_PATH/pgsql/lib/libpq.so ]; then ln -s $INSTALL_PATH/pgsql/lib/libpq.so.5.7 $INSTALL_PATH/pgsql/lib/libpq.so; fi
+if [ ! -f $INSTALL_PATH/pgsql/lib/libpq.so.5 ]; then ln -s $INSTALL_PATH/pgsql/lib/libpq.so.5.7 $INSTALL_PATH/pgsql/lib/libpq.so.5; fi
 
 AGENS_HOME="$INSTALL_PATH"
 agens_passwd_file=$AGENS_HOME/pgsql/dbuser_passwd
 password="$agens.password"
 port="$agens_sql.port"
 DATA_DIR="$agens_sql.data_path"
+LD_LIBRARY_PATH=$AGENS_HOME/pgsql/lib
 
 # create password file
 echo "$password" > $agens_passwd_file
@@ -14,21 +23,21 @@ chmod 600 $agens_passwd_file
 
 # initialize agens database
 if [ -z $password ]; then
-	LD_LIBRARY_PATH=$AGENS_HOME/pgsql/lib $AGENS_HOME/pgsql/bin/initdb -U agens -D $DATA_DIR
+	$AGENS_HOME/pgsql/bin/initdb -U agens -D $DATA_DIR
 else
-	LD_LIBRARY_PATH=$AGENS_HOME/pgsql/lib $AGENS_HOME/pgsql/bin/initdb --pwfile=$agens_passwd_file -A md5 -U agens -D $DATA_DIR
+	$AGENS_HOME/pgsql/bin/initdb --pwfile=$agens_passwd_file -A md5 -U agens -D $DATA_DIR
 fi
 
 # edit postgresql.conf
 if [ $port -ne "5456" ]; then
-    sed -e 's/#port = 5456/port = $agens_sql.port/g' $DATA_DIR/postgresql.conf > $DATA_DIR/postgresql.conf
-    mv $DATA_DIR/postgresql.conf $DATA_DIR/postgresql.conf
+    sed -e 's/#port = 5456/port = $agens_sql.port/g' $DATA_DIR/postgresql.conf > $DATA_DIR/postgresql.conf.$$
+    mv $DATA_DIR/postgresql.conf.$$ $DATA_DIR/postgresql.conf
     chmod 600 $DATA_DIR/postgresql.conf
 fi
 
 
 # operate server
-LD_LIBRARY_PATH=$AGENS_HOME/pgsql/lib $AGENS_HOME/pgsql/bin/pg_ctl -w -D $DATA_DIR -l $DATA_DIR/server_log.txt start
+$AGENS_HOME/pgsql/bin/pg_ctl -w -D $DATA_DIR -l $DATA_DIR/server_log.txt start
 
 # createdb
 if [ -z $password ]; then
@@ -43,5 +52,3 @@ rm -f $agens_passwd_file
 
 # run psql
 # $AGENS_HOME/pgsql/bin/psql -p $port -U agens agens
-
-
