bfcd3ed2bc8956b659dccafbe2c19f5f151621e2
mesh <mesh@localhost.localdomain>
2015-06-16 17:35:28 +0900

    remove .sh files on target machine

--- res/installer/compile_powa_archivist.sh
+++ res/installer/compile_powa_archivist.sh
@@ -16,6 +16,7 @@ $INSTALL_PATH/pgsql/bin/psql -U agens -p $agens_sql.port -d powa -c "CREATE EXTE
 $INSTALL_PATH/pgsql/bin/psql -U agens -p $agens_sql.port -d powa -c "CREATE EXTENSION pg_stat_statements;"
 $INSTALL_PATH/pgsql/bin/psql -U agens -p $agens_sql.port -d powa -c "CREATE EXTENSION pg_qualstats;"
 $INSTALL_PATH/pgsql/bin/psql -U agens -p $agens_sql.port -d powa -c "CREATE EXTENSION pg_stat_kcache;"
+$INSTALL_PATH/pgsql/bin/psql -U agens -p $agens_sql.port -d powa -c "CREATE EXTENSION powa;"
 $INSTALL_PATH/pgsql/bin/psql -U agens -p $agens_sql.port -d powa -c "SELECT powa_qualstats_register();"
 $INSTALL_PATH/pgsql/bin/psql -U agens -p $agens_sql.port -d powa -c "SELECT powa_kcache_register();"
 
