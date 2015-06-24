fd23025f0b002623bea207c6584ecdc796057f4c
mesh <mesh@localhost.localdomain>
2015-06-24 15:22:32 +0900

    remove password

--- res/installer/compile_pgsql.sh
+++ res/installer/compile_pgsql.sh
@@ -12,20 +12,11 @@ if [ ! -f $INSTALL_PATH/pgsql/lib/libpq.so.5 ]; then ln -s $INSTALL_PATH/pgsql/l
 
 AGENS_HOME="$INSTALL_PATH"
 agens_passwd_file=$AGENS_HOME/pgsql/dbuser_passwd
-password="$agens.password"
 port="$agens_sql.port"
 DATA_DIR="$agens_sql.data_path"
 
-# create password file
-echo "$password" > $agens_passwd_file
-chmod 600 $agens_passwd_file
-
 # initialize agens database
-if [ -z $password ]; then
-	$AGENS_HOME/pgsql/bin/initdb -U agens -D $DATA_DIR
-else
-	$AGENS_HOME/pgsql/bin/initdb --pwfile=$agens_passwd_file -A md5 -U agens -D $DATA_DIR
-fi
+$AGENS_HOME/pgsql/bin/initdb -U agens -D $DATA_DIR
 
 # edit postgresql.conf
 if [ $port -ne "5456" ]; then
@@ -39,15 +30,5 @@ fi
 LD_LIBRARY_PATH=$AGENS_HOME/pgsql/lib:$LD_LIBRARY_PATH $AGENS_HOME/pgsql/bin/pg_ctl -w -D $DATA_DIR -l $DATA_DIR/server_log.txt start
 
 # createdb
-if [ -z $password ]; then
-	$AGENS_HOME/pgsql/bin/createdb -p $port -U agens agens
-else
-	$AGENS_HOME/pgsql/bin/createdb -p $port -U agens agens < $agens_passwd_file
-fi
+$AGENS_HOME/pgsql/bin/createdb -p $port -U agens agens
 
-# remove password file
-rm -f $agens_passwd_file
-
-
-# run psql
-# $AGENS_HOME/pgsql/bin/psql -p $port -U agens agens
